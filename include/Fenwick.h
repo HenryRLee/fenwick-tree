8583c44cc543084b0fa41c81ff7f38e04d4fde63
HenryLee <lee0906@hotmail.com>
2018-12-13 21:35:45 +0800

    Initial commit

---
+++ include/Fenwick.h
@@ -0,0 +1,38 @@
+#include <vector>
+
+template <class T>
+class Fenwick {
+public:
+  class Node;
+
+  Fenwick() { }
+  Fenwick(int size);
+
+  void resize(int size) {
+    size_ = size;
+  }
+
+  int size() {
+    return size_;
+  }
+
+  Node operator[] (int index) {
+    return Node(this, index);
+  }
+
+  class Node {
+    public:
+      Node(Fenwick &tree, int index) : tree_(tree), index_(index) { }
+    private:
+      Fenwick &tree_;
+      int index_;
+  };
+
+private:
+  std::vector<T> data;
+  std::vector<T> tree;
+
+  int size_;
+
+  friend class Node;
+};
---
+++ test/unit_test.cc
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(test, test) {
+  ASSERT_EQ(0, 0);
+}
+
