ad63749b28b584813df2b19c58e6ed63d79b1b23
HenryLee <lee0906@hotmail.com>
2018-12-28 16:17:38 +1100

    Change the test code sum.cc

--- test/sum.cc
+++ test/sum.cc
@@ -4,31 +4,33 @@
 namespace fenwick {
 
 TEST(SumTest, OneElement) {
-  fenwick<int> tree = fenwick<int>(10);
+  fenwick<int> tree = fenwick<int>(1);
 
-  tree[0] += 1;
+  tree[0] = 1;
 
-  ASSERT_EQ(static_cast<int>(tree.sum(1)), 1);
+  ASSERT_EQ(tree.sum(1), 1);
 }
 
 TEST(SumTest, ThreeElements) {
-  fenwick<int> tree = fenwick<int>(10);
+  fenwick<int> tree = fenwick<int>(3);
 
-  tree[0] += 1;
-  tree[1] += 5;
-  tree[2] += 9;
+  tree[0] = 1;
+  tree[1] = 5;
+  tree[2] = 9;
 
-  ASSERT_EQ(static_cast<int>(tree.sum(3)), 15);
+  ASSERT_EQ(tree.sum(3), 15);
 }
 
 TEST(SumTest, ThreeElementsWithNegativeNumber) {
-  fenwick<int> tree = fenwick<int>(10);
+  fenwick<int> tree = fenwick<int>(3);
 
-  tree[0] += 1;
-  tree[1] += -9;
-  tree[2] += 5;
+  tree[0] = 1;
+  tree[1] = -9;
+  tree[2] = 5;
 
-  ASSERT_EQ(static_cast<int>(tree.sum(3)), -3);
+  ASSERT_EQ(tree.sum(3), -3);
+}
+
 }
 
 }
