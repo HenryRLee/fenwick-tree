1d48410644e065c1dfdc62354989b241be0bc2ab
Henry Lee <lee0906@hotmail.com>
2019-02-07 17:45:46 +1100

    Add test code for iterators

---
+++ test/iterator.cc
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "fenwick/fenwick.h"
+
+namespace fenwick {
+
+TEST(IteratorTest, BeginEnd) {
+  fenwick<int> tree = fenwick<int>(10);
+
+  fenwick<int>::iterator it;
+  it = tree.begin();
+  it = tree.end();
+}
+
+TEST(IteratorTest, Iterate) {
+  fenwick<int> tree = fenwick<int>(5);
+
+  for (int i = 0; i < 5; i++) {
+    tree[i] = i;
+  }
+
+  int c = 0;
+  for (auto it = tree.begin(); it != tree.end(); it++, c++) {
+    ASSERT_EQ(c, *it);
+  }
+}
+
+}
