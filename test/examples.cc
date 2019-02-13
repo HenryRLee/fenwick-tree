b7007fe87140ebace51cee7eb4dca09fde7da286
Henry Lee <lee0906@hotmail.com>
2019-02-13 22:52:05 +1100

    Fix the test code in examples

--- test/examples.cc
+++ test/examples.cc
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
 #include "fenwick/fenwick.h"
-#include <iostream>
 
 TEST(Examples, ExampleOne) {
   fenwick::fenwick<int> array(5); // initiate an array with 5 elements
@@ -9,7 +8,7 @@ TEST(Examples, ExampleOne) {
   array[1] = 3;
   array[2] = 5;
   array[3] = 7;
-  array[5] = 9;
+  array[4] = 9;
 
   ASSERT_EQ(array.sum(3), 9); // the sum of the first three elements
   ASSERT_EQ(array.sum(1, 4), 15); // the sum of the range [1, 4)
