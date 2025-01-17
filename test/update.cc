6e2b08a020176cf571b0991667631167c7da189b
Henry Lee <lee0906@hotmail.com>
2025-01-17 15:22:20 +1100

    Add sum tests after update

--- test/update.cc
+++ test/update.cc
@@ -7,24 +7,51 @@ TEST(UpdateTest, Increment) {
   fenwick<int> tree = fenwick<int>(10);
 
   tree[0] += 1;
-
   ASSERT_EQ(tree[0], 1);
+
+  tree[1] += 2;
+  ASSERT_EQ(tree[1], 2);
+
+  tree[2] += -1;
+  ASSERT_EQ(tree[2], -1);
+
+  ASSERT_EQ(tree.sum(1), 1);
+  ASSERT_EQ(tree.sum(2), 3);
+  ASSERT_EQ(tree.sum(3), 2);
 }
 
 TEST(UpdateTest, Decrement) {
   fenwick<int> tree = fenwick<int>(10);
 
   tree[0] -= 3;
-
   ASSERT_EQ(tree[0], -3);
+
+  tree[1] -= -2;
+  ASSERT_EQ(tree[1], 2);
+
+  tree[2] -= 4;
+  ASSERT_EQ(tree[2], -4);
+
+  ASSERT_EQ(tree.sum(1), -3);
+  ASSERT_EQ(tree.sum(2), -1);
+  ASSERT_EQ(tree.sum(3), -5);
 }
 
 TEST(UpdateTest, Assign) {
   fenwick<int> tree = fenwick<int>(10);
 
-  tree[0] = 2;
+  tree[0] = 1;
+  ASSERT_EQ(tree[0], 1);
 
-  ASSERT_EQ(tree[0], 2);
+  tree[1] = 2;
+  ASSERT_EQ(tree[1], 2);
+
+  tree[2] = -7;
+  ASSERT_EQ(tree[2], -7);
+
+  ASSERT_EQ(tree.sum(1), 1);
+  ASSERT_EQ(tree.sum(2), 3);
+  ASSERT_EQ(tree.sum(3), -4);
 }
 
 }
